e0ce9e9af026c094e4af6672816e343cc031a889
Shreeprabha N <96766072+shreeprabha1217@users.noreply.github.com>
2024-06-19 11:35:06 +0530

    Create Diagonal_sum.java

---
+++ Diagonal_sum.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrixA = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrixA.length;
+        int sum1=0,sum2=0;
+
+        for (int i = 0; i < rows; i++) {
+            for(int j=0;j<rows;j++){
+                if(i==j){
+                    sum1+=matrixA[i][j];
+                }
+                else if((i+j)==(rows-1)){
+                    sum2+=matrixA[i][j];
+                }
+            }
+        }
+            
+
+        System.out.println("Sum of diagonal elements: " + sum1);
+    }
+}
