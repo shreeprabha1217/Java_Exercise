185f80dff3716e1f3840a738bb668a2df8b8b848
Shreeprabha N <96766072+shreeprabha1217@users.noreply.github.com>
2024-09-04 09:47:24 +0530

    Create Nth_root_of_m.java

---
+++ Nth_root_of_m.java
@@ -0,0 +1,31 @@
+class Solution
+{
+    public int func(int mid,int n,int m){
+        long ans=1;
+        for(int i=0;i<n;i++){
+            ans=ans*mid;
+            if(ans>m)
+                return 2;
+        }
+        if(ans==m){
+            return 1;
+        }
+        return 0;
+    }
+    public int NthRoot(int n, int m)
+    {
+        int low=1,high=m;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int midN=func(mid,n,m);
+            if(midN==1){
+                return mid;
+            }
+            else if(midN==0){
+                low=mid+1;
+            }else
+                high=mid-1;
+        }    
+        return -1;// code here
+    }
+}
